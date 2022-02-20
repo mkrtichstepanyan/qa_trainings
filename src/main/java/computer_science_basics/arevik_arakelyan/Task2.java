2b125537193c0e77e59099dd8baf3cb102f970af
Arevik Araqelyan <arevik.arakelyan.1988@gmail.com>
2022-02-21 01:07:45 +0400

    My 1st QA homework

---
+++ src/main/java/computer_science_basics/arevik_arakelyan/Task2.java
@@ -0,0 +1,20 @@
+package computer_science_basics.arevik_arakelyan;
+
+public class Task2 {
+
+   
+
+    public static void main(String[] args) {
+        int[] twinsArray = {4, 2, 5, -4, 3, 0, 3, 6, 1, 8, -2, -2, -3, -6, -3, -6, 5, 4, 2, -1, -5, 0, 6, -1, 1, -5, -4};
+        System.out.println("The single number is -> " + findSingleNumber(twinsArray));
+    }
+
+    public static int findSingleNumber(int[] array) {
+        int[] arr = new int[]{4, 2, 5, -4, 3, 0, 3, 6, 1, 8, -2, -2, -3, -6, -3, -6, 5, 4, 2, -1, -5, 0, 6, -1, 1, -5, -4};
+        int res = 0;
+        for (int i: arr) {
+            res ^= i;
+        }
+        return res;
+    }
+}
