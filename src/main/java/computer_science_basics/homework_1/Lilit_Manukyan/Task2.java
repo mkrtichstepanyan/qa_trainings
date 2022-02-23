cb2c52e2f6fbafa979917cf9a282df5edb1ed25d
Lilit Manukyan <lilitmanukyan888@gmail.com>
2022-02-23 19:41:02 +0400

    homework

--- src/main/java/computer_science_basics/homework_1/Lilit_Manukyan/Task2.java
+++ src/main/java/computer_science_basics/homework_1/Lilit_Manukyan/Task2.java
@@ -15,10 +15,14 @@ public class Task2 {
     }
 
     public static int findSingleNumber(int[] array) {
+    
         int singleNumber = array[0];
+        
         for (int i = 1; i < array.length ; i++) {
             singleNumber ^= array[i];
         }
-        return singleNumber;
+        
+        return singleNumber;   
+          
     }
 }
