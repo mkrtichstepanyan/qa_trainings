8c5cdf110c910fa0cf0bbbf073aee92e514b6951
mkrtichstepanyan <32538476+mkrtichstepanyan@users.noreply.github.com>
2022-02-24 16:14:23 +0400

    fixed

--- src/main/java/computer_science_basics/homework_1/Task2.java
+++ src/main/java/computer_science_basics/homework_1/Task2.java
@@ -10,7 +10,7 @@ public class Task2 {
     */
 
     public static void main(String[] args) {
-        int[] twinsArray = {4, 2, 5, -4, 3, 0, 3, 6, 1, 8, -2, -2, -3, -6, -3, -6, 5, 4, 2, -1, -5, 0, 6, -1, 1, -5, 4};
+        int[] twinsArray = {4, 2, 5, -4, 3, 0, 3, 6, 1, 8, -2, -2, -3, -6, -3, -6, 5, 4, 2, -1, -5, 0, 6, -1, 1, -5, -4};
         System.out.println("The single number is -> " + findSingleNumber(twinsArray));
     }
 
@@ -19,3 +19,4 @@ public class Task2 {
         return 0;
     }
 }
+
