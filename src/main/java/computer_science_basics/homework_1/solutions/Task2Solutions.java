12f9c71d3dadb8eb4f34fe30dc33c3e2e3109b3d
mkrtichstepanyan <32538476+mkrtichstepanyan@users.noreply.github.com>
2022-02-28 21:24:58 +0400

    Task Solutions

---
+++ src/main/java/computer_science_basics/homework_1/solutions/Task2Solutions.java
@@ -0,0 +1,24 @@
+package computer_science_basics.homework_1.solutions;
+
+public class Task2Solutions {
+
+    /* TODO: Task 2
+        Given an array of integers ex. [1, 4, -7, 0, 4, -7, 0 ]
+        All of them has a twin in the same array 4 & 4, -7 & -7, 0 & 0
+        Find and print a single integer which has no a twin (in this array it is 1)
+        Use as few iterations as you can, try to find THE BEST solution
+    */
+
+    public static void main(String[] args) {
+        int[] twinsArray = {4, 2, 5, -4, 3, 0, 3, 6, 1, 7, -2, -2, -3, -6, -3, -6, 5, 4, 2, -1, -5, 0, 6, -1, 1, -5, -4};
+        System.out.println("The single number is -> " + findSingleNumber(twinsArray));
+    }
+
+    public static int findSingleNumber(int[] array) {
+        int result = 0;
+        for (int j : array) {
+            result ^= j;
+        }
+        return result;
+    }
+}
