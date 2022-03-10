f5a4b2d91819516bcb3ba6a45c9cc37938eb97e0
anidarbinyan <anidarbinyan14@gmail.com>
2022-03-11 01:25:25 +0400

    added function armStrongNumber

--- src/main/java/computer_science_basics/homework_2/ani_darbinyan/Task3.java
+++ src/main/java/computer_science_basics/homework_2/ani_darbinyan/Task3.java
@@ -324,7 +324,20 @@ public class Task3 {
                 digit2,  // To hold second digit (Tens) of number
                 digit3;  // To hold third digit (Hundreds) of number
 
+        for (int number = 1; number <= 500; number++) {
+            int temp = number;
+            digit1 = temp % 10;
 
+            temp = temp / 10;
+            digit2 = temp % 10;
+
+            temp = temp / 10;
+            digit3 = temp % 10;
+
+            if (digit1 * digit1 * digit1 + digit2 * digit2 * digit2 + digit3 * digit3 * digit3 == number) {
+                System.out.println(number);
+            }
+        }
     }
 
     /* TODO: Question 13
