b914fb670157ae0b46f3a7ca302d88d21f0abd44
Arevik Araqelyan <arevik.arakelyan.1988@gmail.com>
2022-04-05 16:15:39 +0400

    all changes is done

--- src/main/java/arrays/homework_3/SortingAlgorithms.java
+++ src/main/java/arrays/homework_3/SortingAlgorithms.java
@@ -1,67 +1,10 @@
 package arrays.homework_3;
-import java.util.Arrays;
 
 public class SortingAlgorithms {
-/* TODO: Write algorithms below
-        1. Insertion Sort
-        2. Selection Sort
-        3. Bubble Sort
-     */
+   /* TODO: Write algorithms below
+       1. Insertion Sort
+       2. Selection Sort
+       3. Bubble Sort
+    */
 
-    public static void main(String args[]) {
-        int [] arr = {5, 3, 4, 2, 15, 10,40,1};
-        System.out.println("Insertion sorting algorithm");
-        arr = insertionSort(arr);
-        System.out.println(Arrays.toString(arr));
-        System.out.println("Selection sorting algorithm");
-        arr = selectionSort(arr );
-        System.out.println(Arrays.toString(arr));
-        System.out.println("Bubble sorting algorithm");
-        arr = bubbleSort(arr );
-        System.out.println(Arrays.toString(arr));
-    }
-
-    public static int[] insertionSort(int arr[]) {
-        for (int i = 1; i < arr.length; i++) {
-            int temp = arr[i];
-            int j;
-            for (j = i; j > 0 && arr[j - 1] > temp; j--) {
-                arr[j] = arr[j - 1];
-            }
-            arr[j] = temp;
-        }
-        return arr;
-    }
-    //selection sorting
-        public static int[] selectionSort(int[] arr) {
-
-            for (int i = 0; i < arr.length - 1; i++) {
-                int index = i;
-                for (int j = i + 1; j < arr.length; j++)
-                    if (arr[j] < arr[index])
-                        index = j;
-
-                int smallerNumber = arr[index];
-                arr[index] = arr[i];
-                arr[i] = smallerNumber;
-            }
-            return arr;
-        }
-
-        //bubble sorting
-
-    public static int[] bubbleSort(int arr[])
-    {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length-1-i; j++) {
-                if(arr[j]>arr[j+1])
-                {
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
-                }
-            }
-        }
-        return arr;
-    }
 }
